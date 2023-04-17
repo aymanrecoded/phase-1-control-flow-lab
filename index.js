4a889cc882a30bddaac1b3a77f3b768c52b33525
aymanattar <aymanxman19999@gmail.com>
2023-04-17 04:33:49 -0700

    Done

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let result;
+
+  if (feet <= 400) {
+    result = "This one is on me!";
+  } else if (feet > 400 && feet <= 2000) {
+    result = "That will be twenty bucks.";
+  } else if (feet > 2000 && feet <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else {
+    result = "No can do.";
+  }
+
+  return result;
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city) {
   // Write your code here!
+  const zoom = city === "NYC" ? ("Ok, sounds good.") : ("No go.");
+  return zoom;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(response) {
   // Write your code here!
+  let responseType;
+  switch (response) {
+    case 'generous':
+      responseType = "Thank you so much."
+      break;
+    case 'not as generous':
+      responseType = "Thank you."
+      break;
+    default:
+      responseType = "Bye."
+      break;
+  }
+  return responseType
 }
